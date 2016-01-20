5b67e0ca54ab3e118d998d062060ee67c6ab0d74
Manu Mtz.-Almeida <manu.valladolid@gmail.com>
2016-01-20 19:29:38 +0100

    Adds BasicMsg()

--- common/utils.go
+++ common/utils.go
@@ -1,5 +1,9 @@
 package common
 
+func BasicMsg(eventName string, data interface{}) Msg {
+	return NewMsg(eventName, "", "", data)
+}
+
 func Emit(conn Connection, eventName string, data interface{}) error {
-	return conn.Send(NewMsg(eventName, "", "", data))
+	return conn.Send(BasicMsg(eventName, data))
 }
